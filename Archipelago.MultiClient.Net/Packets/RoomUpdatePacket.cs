1710a1c66957c8d339a989ce32e7450ef14133ce
Hussein Farran <hmfarran@gmail.com>
2021-02-24 22:09:34 -0500

    RoomUpdatePacket now appropriately derives from ArchipelagoPacketBase

--- Archipelago.MultiClient.Net/Packets/RoomUpdatePacket.cs
+++ Archipelago.MultiClient.Net/Packets/RoomUpdatePacket.cs
@@ -2,7 +2,7 @@
 
 namespace Archipelago.MultiClient.Net.Packets
 {
-    public class RoomUpdatePacket
+    public class RoomUpdatePacket : ArchipelagoPacketBase
     {
         [JsonProperty("hint_points")]
         public int HintPoints { get; set; }
