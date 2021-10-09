82e0e078b1d7e2e0d92a568ae6003f45811d6eb5
Jarno Westhof <jarnowesthof@gmail.com>
2021-10-09 15:37:51 +0200

    Fixed missmatching indentations

--- Archipelago.MultiClient.Net/Enums/Permissions.cs
+++ Archipelago.MultiClient.Net/Enums/Permissions.cs
@@ -7,9 +7,9 @@ namespace Archipelago.MultiClient.Net.Enums
 	[JsonConverter(typeof(StringEnumConverter))]
 	[Flags]
 	public enum Permissions
-    {
-        Enabled = 1 << 0,
+	{
+		Enabled = 1 << 0,
 		Goal = 1 << 1,
 		Auto = 1 << 2
-    }
+	}
 }
