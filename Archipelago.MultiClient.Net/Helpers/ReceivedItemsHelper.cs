194861274737ddfc44b805b9d5cb81fe4efbff8b
Hussein Farran <hmfarran@gmail.com>
2021-10-21 20:09:43 -0400

    More comment on my incompetence.

--- Archipelago.MultiClient.Net/Helpers/ReceivedItemsHelper.cs
+++ Archipelago.MultiClient.Net/Helpers/ReceivedItemsHelper.cs
@@ -83,6 +83,7 @@ namespace Archipelago.MultiClient.Net.Helpers
         {
             if (!VerifyDataPackageReceived())
             {
+                // Need to wait until the DataPackage comes back.
                 AwaitDataPackage();
             }
 
