a721bf15c43c8670be49e7f7231dbd35178c258e
Jarno Westhof <jarnowesthof@gmail.com>
2021-12-04 00:53:24 +0100

    Added new types to JsonMessagePartType

--- Archipelago.MultiClient.Net/Enums/JsonMessagePartType.cs
+++ Archipelago.MultiClient.Net/Enums/JsonMessagePartType.cs
@@ -2,8 +2,49 @@
 {
     public enum JsonMessagePartType
     {
+        /// <summary>
+        /// Regular text content. this is the default type and as such may be omitted.
+        /// </summary>
+        Text,
+
+        /// <summary>
+        /// Player ID of someone on your team, should be resolved to Player Name
+        /// </summary>
         PlayerId,
+
+        /// <summary>
+        /// Player Name, could be a player within a multiplayer game or from another team, not ID resolvable
+        /// </summary>
+        PlayerName,
+
+        /// <summary>
+        /// Item ID, should be resolved to Item Name
+        /// </summary>
         ItemId,
-        LocationId
+
+        /// <summary>
+        /// Item Name, not currently used over network, but supported by reference Clients.
+        /// </summary>
+        ItemName,
+
+        /// <summary>
+        /// Location ID, should be resolved to Location Name
+        /// </summary>
+        LocationId,
+
+        /// <summary>
+        /// Location Name, not currently used over network, but supported by reference Clients.
+        /// </summary>
+        LocationName,
+
+        /// <summary>
+        /// Entrance Name. No ID mapping exists.
+        /// </summary>
+        EntranceName,
+
+        /// <summary>
+        /// Regular text that should be colored. Only type that will contain color data.
+        /// </summary>
+        Color
     }
 }
