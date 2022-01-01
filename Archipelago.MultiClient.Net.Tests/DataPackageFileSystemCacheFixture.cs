3ef19b7d143dc64166b1ae75dee7d9267a5d42e9
Jarno Westhof <jarnowesthof@gmail.com>
2022-01-01 12:32:52 +0100

    Added unittests

---
+++ Archipelago.MultiClient.Net.Tests/DataPackageFileSystemCacheFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archipelago.MultiClient.Net.Tests
+{
+    [TestFixture]
+    public class DataPackageFileSystemCacheFixture
+    {
+        [Test]
+        public void Test(){
+            Assert.IsFalse(false);
+        }
+    }
+}
