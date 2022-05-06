b4bb3420514e21457467ddb89cfd88e6ffcd5c81
CaitSith2 <d_good@caitsith2.com>
2022-05-06 03:01:50 -0700

    Add Slot Info Data
    Needed for proper support of Item Links.

---
+++ Archipelago.MultiClient.Net/Models/NetworkSlot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Archipelago.MultiClient.Net.Enums;
+using Newtonsoft.Json;
+
+namespace Archipelago.MultiClient.Net.Models
+{
+    public struct NetworkSlot
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("game")]
+        public string Game { get; set; }
+        [JsonProperty("type")]
+        public SlotType Type { get; set; }
+        [JsonProperty("group_members")]
+        public List<int> GroupMembers { get; set; }
+    }
+}
