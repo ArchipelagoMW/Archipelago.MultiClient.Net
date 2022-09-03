3fef5aa5196a051a0f682b6241da6d670c29275b
Hussein Farran <hmfarran@gmail.com>
2022-09-03 15:56:46 -0400

    Add unit tests for ConcurrentQueue.IsEmpty

---
+++ Archipelago.MultiClient.Net35.Tests/ConcurrentCollectionsTests.cs
@@ -0,0 +1,30 @@
+﻿using Archipelago.MultiClient.Net.ConcurrentCollection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Archipelago.MultiClient.Net35.Tests
+{
+    [TestFixture]
+    public class ConcurrentCollectionsFixture
+    {
+        [Test]
+        public void ConcurrentQueue_HasNoItems_GetIsEmpty_ReturnsTrue()
+        {
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+
+            Assert.That(queue.IsEmpty, Is.True);
+        }
+
+        [Test]
+        public void ConcurrentQueue_HasItems_GetIsEmpty_ReturnsFalse()
+        {
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+            queue.Enqueue(1);
+
+            Assert.That(queue.IsEmpty, Is.False);
+        }
+    }
+}
---
+++ Archipelago.MultiClient.Net35.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Archipelago.MultiClient.Net35.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Archipelago.MultiClient.Net35.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("212bddd7-d796-462a-878a-626cf5fe005c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
