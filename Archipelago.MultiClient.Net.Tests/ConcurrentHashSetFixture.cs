555c81465995b7046f4d193c43fc3a1b12edb869
Hussein Farran <hmfarran@gmail.com>
2022-10-20 03:45:28 -0500

    Increase concurrentcollections test coverage (#68)
    * Prevent exceptions when ConcurrentQueue is used in expected ways.
    
    Add more tests.
    
    * Code tidying.
    
    * Include tests for ConcurrentList and ConcurrentHashSet.

---
+++ Archipelago.MultiClient.Net.Tests/ConcurrentHashSetFixture.cs
@@ -0,0 +1,85 @@
+﻿using Archipelago.MultiClient.Net.ConcurrentCollection;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Archipelago.MultiClient.Net.Tests
+{
+    [TestFixture]
+    public class ConcurrentHashSetFixture
+    {
+        [Test]
+        public void ConcurrentHashSet_TryAdd_ItemIsntAlreadyPresent_ReturnsTrue()
+        {
+            var hashset = new ConcurrentHashSet<int>();
+            
+            Assert.That(hashset.TryAdd(1), Is.True);
+        }
+
+        [Test]
+        public void ConcurrentHashSet_TryAdd_ItemIsAlreadyPresent_ReturnsFalse()
+        {
+            var hashset = new ConcurrentHashSet<int>();
+            hashset.TryAdd(1);
+
+            Assert.That(hashset.TryAdd(1), Is.False);
+        }
+
+        [Test]
+        public void ConcurrentHashSet_Contains_ItemIsntAlreadyPresent_ReturnsFalse()
+        {
+            var hashset = new ConcurrentHashSet<int>();
+
+            Assert.That(hashset.Contains(1), Is.False);
+        }
+
+        [Test]
+        public void ConcurrentHashSet_Contains_ItemIsAlreadyPresent_ReturnsTrue()
+        {
+            var hashset = new ConcurrentHashSet<int>();
+            hashset.TryAdd(1);
+
+            Assert.That(hashset.Contains(1), Is.True);
+        }
+
+        [Test]
+        public void ConcurrentHashSet_ToArray_ReturnsCorrectArrayContents()
+        {
+            var hashset = new ConcurrentHashSet<int>();
+            hashset.TryAdd(1);
+            hashset.TryAdd(2);
+
+            Assert.That(hashset.ToArray(), Is.EquivalentTo(Enumerable.Range(1, 2)));
+        }
+
+        [Test]
+        public void ConcurrentHashSet_UnionWith_OtherSet_ProducesCorrectUnion()
+        {
+            var hashset = new ConcurrentHashSet<int>();
+            hashset.TryAdd(1);
+            hashset.UnionWith(new int[] { 1, 2, 3 });
+
+            Assert.That(hashset.ToArray(), Is.EquivalentTo(Enumerable.Range(1,3)));
+        }
+
+        [Test]
+        public void ConcurrentHashSet_AsToReadOnlyCollection_ProducesCorrectContents()
+        {
+            var hashset = new ConcurrentHashSet<int>();
+            hashset.UnionWith(new int[] { 1, 2, 3 });
+
+            Assert.That(hashset.AsToReadOnlyCollection(), Is.EquivalentTo(Enumerable.Range(1, 3)));
+        }
+
+        [Test]
+        public void ConcurrentHashSet_AsToReadOnlyCollectionExcept_ProducesCorrectContents()
+        {
+            var hashset = new ConcurrentHashSet<int>();
+            hashset.UnionWith(new int[] { 1, 2, 3 });
+
+            var other = new ConcurrentHashSet<int>();
+            other.TryAdd(3);
+
+            Assert.That(hashset.AsToReadOnlyCollectionExcept(other), Is.EquivalentTo(Enumerable.Range(1, 2)));
+        }
+    }
+}
---
+++ Archipelago.MultiClient.Net.Tests/ConcurrentListFixture.cs
@@ -0,0 +1,54 @@
+﻿using Archipelago.MultiClient.Net.ConcurrentCollection;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Archipelago.MultiClient.Net.Tests
+{
+    [TestFixture]
+    public class ConcurrentListFixture
+    {
+        [Test]
+        public void ConcurrentList_AddItem_CorrectlyAddsItem()
+        {
+            var list = new ConcurrentList<int>();
+            list.Add(1);
+
+            Assert.That(list.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ConcurrentList_Count_ReturnsCorrectCount()
+        {
+            var list = new ConcurrentList<int>();
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+
+            Assert.That(list.Count, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void ConcurrentList_AsReadOnlyCollection_ReturnsCorrectContents()
+        {
+            var list = new ConcurrentList<int>();
+            list.Add(1);
+
+            Assert.That(list.AsReadOnlyCollection(), Is.EquivalentTo(Enumerable.Range(1,1)));
+        }
+
+        [Test]
+        public void ConcurrentList_Clear_RemovesAllContents()
+        {
+            var list = new ConcurrentList<int>();
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+            list.Clear();
+
+            Assert.That(list.Count, Is.Zero);
+        }
+    }
+}
