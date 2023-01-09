eca964c5e23205b7f910d59eee674113f7ed0ce0
Jarno Westhof <jarnowesthof@gmail.com>
2023-01-09 21:12:20 +0100

    Added Method to track hints using the data storage

--- Archipelago.MultiClient.Net.Tests/DataStorageWrapperFixture.cs
+++ Archipelago.MultiClient.Net.Tests/DataStorageWrapperFixture.cs
@@ -7,6 +7,7 @@ using NSubstitute;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -41,6 +42,8 @@ namespace Archipelago.MultiClient.Net.Tests
 			ExecuteAsyncWithDelay(
 				() => { hints = sut.GetHints(); },
 				() => RaiseRetrieved(socket, "_read_hints_2_8", JArray.FromObject(availableHints)));
+
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_hints_2_8"));
 
 			Assert.IsNotNull(hints);
 
@@ -95,6 +98,116 @@ namespace Archipelago.MultiClient.Net.Tests
 				() => { hints = sut.GetHintsAsync().Result; },
 				() => RaiseRetrieved(socket, "_read_hints_3_7", JArray.FromObject(availableHints)));
 #endif
+
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_hints_3_7"));
+
+			Assert.IsNotNull(hints);
+
+			Assert.That(hints.Length, Is.EqualTo(availableHints.Length));
+			Assert.That(hints[0].Entrance, Is.EqualTo(availableHints[0].Entrance));
+			Assert.That(hints[0].FindingPlayer, Is.EqualTo(availableHints[0].FindingPlayer));
+			Assert.That(hints[0].Found, Is.EqualTo(availableHints[0].Found));
+			Assert.That(hints[0].ItemFlags, Is.EqualTo(availableHints[0].ItemFlags));
+			Assert.That(hints[0].ItemId, Is.EqualTo(availableHints[0].ItemId));
+			Assert.That(hints[0].ReceivingPlayer, Is.EqualTo(availableHints[0].ReceivingPlayer));
+		}
+
+		[Test]
+		public void TrackHints_true_should_call_callback_for_hint_changes_and_request_initial_value()
+		{
+			var socket = Substitute.For<IArchipelagoSocketHelper>();
+			var connectionInfo = Substitute.For<IConnectionInfoProvider>();
+			connectionInfo.Slot.Returns(7);
+			connectionInfo.Team.Returns(3);
+
+			var sut = new DataStorageHelper(socket, connectionInfo);
+
+			int hintsCallbackCount = 0;
+			Hint[] hints = null;
+
+			var availableHints = new[]{
+				new Hint {
+					Entrance = null,
+					FindingPlayer = 9,
+					Found = true,
+					ItemFlags = ItemFlags.None,
+					ItemId = 1337L,
+					ReceivingPlayer = 7
+				}
+			};
+
+			// ReSharper disable once RedundantArgumentDefaultValue
+			ExecuteAsyncWithDelay(
+				() => {
+					sut.TrackHints(t => {
+						hintsCallbackCount++;
+						hints = t;
+					}, true);
+				},
+				() =>
+				{
+					RaiseRetrieved(socket, "_read_hints_3_7", JArray.FromObject(availableHints));
+					RaiseWSetReply(socket, "_read_hints_3_7", JArray.FromObject(availableHints));
+				});
+
+			while (hintsCallbackCount < 2)
+				Thread.Sleep(10);
+			
+			Received.InOrder(() =>
+			{
+				socket.SendPacketAsync(Arg.Is<SetNotifyPacket>(p => p.Keys.FirstOrDefault() == "_read_hints_3_7"));
+				socket.SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_hints_3_7"));
+			});
+
+			Assert.IsNotNull(hints);
+
+			Assert.That(hints.Length, Is.EqualTo(availableHints.Length));
+			Assert.That(hints[0].Entrance, Is.EqualTo(availableHints[0].Entrance));
+			Assert.That(hints[0].FindingPlayer, Is.EqualTo(availableHints[0].FindingPlayer));
+			Assert.That(hints[0].Found, Is.EqualTo(availableHints[0].Found));
+			Assert.That(hints[0].ItemFlags, Is.EqualTo(availableHints[0].ItemFlags));
+			Assert.That(hints[0].ItemId, Is.EqualTo(availableHints[0].ItemId));
+			Assert.That(hints[0].ReceivingPlayer, Is.EqualTo(availableHints[0].ReceivingPlayer));
+		}
+
+		[Test]
+		public void TrackHints_false_should_call_callback_for_hint_changes_but_not_request_initial_value()
+		{
+			var socket = Substitute.For<IArchipelagoSocketHelper>();
+			var connectionInfo = Substitute.For<IConnectionInfoProvider>();
+			connectionInfo.Slot.Returns(8);
+			connectionInfo.Team.Returns(6);
+
+			var sut = new DataStorageHelper(socket, connectionInfo);
+
+			bool hintsCallbackReceived = false;
+			Hint[] hints = null;
+
+			var availableHints = new[]{
+				new Hint {
+					Entrance = null,
+					FindingPlayer = 9,
+					Found = true,
+					ItemFlags = ItemFlags.None,
+					ItemId = 1337L,
+					ReceivingPlayer = 8
+				}
+			};
+
+			ExecuteAsyncWithDelay(
+				() => {
+					sut.TrackHints(t => {
+						hintsCallbackReceived = true;
+						hints = t;
+					}, false);
+				},
+				() => RaiseWSetReply(socket, "_read_hints_6_8", JArray.FromObject(availableHints)));
+
+			while (!hintsCallbackReceived)
+				Thread.Sleep(10);
+			
+			socket.Received().SendPacketAsync(Arg.Is<SetNotifyPacket>(p => p.Keys.FirstOrDefault() == "_read_hints_6_8"));
+			socket.DidNotReceive().SendPacketAsync(Arg.Any<GetPacket>());
 			
 			Assert.IsNotNull(hints);
 
@@ -125,6 +238,8 @@ namespace Archipelago.MultiClient.Net.Tests
 				() => { hintBySlotAndTeam = sut.GetHints(11, 2); },
 				() => RaiseRetrieved(socket, "_read_hints_2_11", JValue.CreateNull()));
 
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_hints_2_11"));
+			
 			Assert.Null(hintsBySlot);
 			Assert.Null(hintBySlotAndTeam);
 		}
@@ -171,6 +286,7 @@ namespace Archipelago.MultiClient.Net.Tests
 				() => { hintBySlotAndTeam = sut.GetHintsAsync(11, 11).Result; },
 				() => RaiseRetrieved(socket, "_read_hints_11_11", JValue.CreateNull()));
 #endif
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_hints_11_11"));
 
 			Assert.Null(hintsBySlot);
 			Assert.Null(hintBySlotAndTeam);
@@ -196,6 +312,8 @@ namespace Archipelago.MultiClient.Net.Tests
 				() => { slotData = sut.GetSlotData(); },
 				() => RaiseRetrieved(socket, "_read_slot_data_4", JObject.FromObject(serverSlotData)));
 
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_slot_data_4"));
+
 			Assert.IsNotNull(slotData);
 
 			Assert.That(slotData["One"], Is.EqualTo(1)); 
@@ -215,6 +333,8 @@ namespace Archipelago.MultiClient.Net.Tests
 			ExecuteAsyncWithDelay(
 				() => { slotData = sut.GetSlotData(99); },
 				() => RaiseRetrieved(socket, "_read_slot_data_99", JValue.CreateNull()));
+
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_slot_data_99"));
 
 			Assert.IsNull(slotData);
 		}
@@ -255,6 +375,8 @@ namespace Archipelago.MultiClient.Net.Tests
 				() => RaiseRetrieved(socket, "_read_slot_data_88", JObject.FromObject(serverSlotData)));
 #endif
 
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_slot_data_88"));
+
 			Assert.IsNotNull(slotData);
 
 			Assert.That(slotData["One"], Is.EqualTo(1));
@@ -290,6 +412,7 @@ namespace Archipelago.MultiClient.Net.Tests
 				() => { slotData = sut.GetSlotDataAsync(88).Result; },
 				() => RaiseRetrieved(socket, "_read_slot_data_88", JValue.CreateNull()));
 #endif
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_slot_data_88"));
 
 			Assert.IsNull(slotData);
 		}
@@ -314,6 +437,8 @@ namespace Archipelago.MultiClient.Net.Tests
 				() => { slotData = sut.GetItemNameGroups(); },
 				() => RaiseRetrieved(socket, "_read_item_name_groups_MY Game", JObject.FromObject(serverItemNameGroups)));
 
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_item_name_groups_MY Game"));
+
 			Assert.IsNotNull(slotData);
 
 			Assert.That(slotData["Group1"][0], Is.EqualTo("A"));
@@ -335,6 +460,8 @@ namespace Archipelago.MultiClient.Net.Tests
 			ExecuteAsyncWithDelay(
 				() => { itemNameGroups = sut.GetItemNameGroups("NOT A REAL GAME"); },
 				() => RaiseRetrieved(socket, "_read_item_name_groups_NOT A REAL GAME", JValue.CreateNull()));
+
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_item_name_groups_NOT A REAL GAME"));
 
 			Assert.IsNull(itemNameGroups);
 		}
@@ -375,6 +502,8 @@ namespace Archipelago.MultiClient.Net.Tests
 				() => RaiseRetrieved(socket, "_read_item_name_groups_Below Zero", JObject.FromObject(serverItemNameGroups)));
 #endif
 
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_item_name_groups_Below Zero"));
+			
 			Assert.IsNotNull(itemNameGroups);
 
 			Assert.That(itemNameGroups["Group1"][0], Is.EqualTo("A"));
@@ -413,6 +542,8 @@ namespace Archipelago.MultiClient.Net.Tests
 				() => RaiseRetrieved(socket, "_read_item_name_groups_YoloTheGame", JValue.CreateNull()));
 #endif
 
+			socket.Received().SendPacketAsync(Arg.Is<GetPacket>(p => p.Keys.FirstOrDefault() == "_read_item_name_groups_YoloTheGame"));
+
 			Assert.IsNull(itemNameGroups);
 		}
 
@@ -430,7 +561,14 @@ namespace Archipelago.MultiClient.Net.Tests
 
 		public static void RaiseRetrieved(IArchipelagoSocketHelper socket, string key, JToken value)
 		{
-			var packet = new RetrievedPacket() { Data = new Dictionary<string, JToken> { { key, value } } };
+			var packet = new RetrievedPacket { Data = new Dictionary<string, JToken> { { key, value } } };
+
+			socket.PacketReceived += Raise.Event<ArchipelagoSocketHelperDelagates.PacketReceivedHandler>(packet);
+		}
+
+		public static void RaiseWSetReply(IArchipelagoSocketHelper socket, string key, JToken value)
+		{
+			var packet = new SetReplyPacket { Key = key, Value = value };
 
 			socket.PacketReceived += Raise.Event<ArchipelagoSocketHelperDelagates.PacketReceivedHandler>(packet);
 		}
