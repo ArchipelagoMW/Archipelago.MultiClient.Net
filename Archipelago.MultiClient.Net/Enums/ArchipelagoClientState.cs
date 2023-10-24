b4a4e59f398ba3ddfd842cf43f1178594ca91426
Zach Parks <zach@alliware.com>
2023-10-24 06:13:01 -0500

    Add CONNECTED ClientState. (#82)
    Technically set automatically by MultiServer, but still can be defined.
    
    https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/network%20protocol.md#client-states

--- Archipelago.MultiClient.Net/Enums/ArchipelagoClientState.cs
+++ Archipelago.MultiClient.Net/Enums/ArchipelagoClientState.cs
@@ -3,6 +3,7 @@
     public enum ArchipelagoClientState : int
     {
         ClientUnknown = 0,
+		ClientConnected = 5,
         ClientReady = 10,
         ClientPlaying = 20,
         ClientGoal = 30
