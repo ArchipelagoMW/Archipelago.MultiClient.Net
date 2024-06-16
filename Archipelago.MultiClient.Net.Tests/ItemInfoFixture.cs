a905e4568c65453b6f9d2906c0520fedc7a2826b
Jarno Westhof <jarnowesthof@gmail.com>
2024-06-17 00:10:27 +0200

    Progress i guess

---
+++ Archipelago.MultiClient.Net.Tests/ItemInfoFixture.cs
@@ -0,0 +1,100 @@
+﻿using Archipelago.MultiClient.Net.DataPackage;
+using Archipelago.MultiClient.Net.Enums;
+using Archipelago.MultiClient.Net.Helpers;
+using Archipelago.MultiClient.Net.Models;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Archipelago.MultiClient.Net.Tests
+{
+	[TestFixture]
+	class ItemInfoFixture
+	{
+		[TestCase(true, true, true)]
+		[TestCase(true, false, true)]
+		[TestCase(true, true, false)]
+		[TestCase(false, true, true)]
+		[TestCase(false, false, true)]
+		[TestCase(false, true, false)]
+		public void Should_serialize_and_deserialize_FullItemInfo(bool scouted, bool fullJson, bool provideSession)
+		{
+			var receiverGame = "ReceiverGame";
+			var senderGame = "SenderGame";
+
+			var networkItem = new NetworkItem() { Item = 20L, Location = 50L, Player = 3, Flags = ItemFlags.NeverExclude };
+
+			var sender = new PlayerInfo {
+				Team = 20,
+				Slot = 3,
+				Alias = "Not defined",
+				Game = scouted ? receiverGame : senderGame,
+				Groups = new[] {
+					new NetworkSlot {
+						Game = "Archipelago",
+						GroupMembers = new[] { 4, 5, 600 },
+						Name = "ItemSink",
+						Type = SlotType.Group
+					}
+				}
+			};
+
+			var currentPlayer = new PlayerInfo
+			{
+				Team = 20,
+				Slot = 7,
+				Alias = "Current",
+				Game = scouted ? senderGame : receiverGame,
+				Groups = new[] {
+					new NetworkSlot {
+						Game = "Archipelago",
+						GroupMembers = new[] { 1 },
+						Name = "CurrentP",
+						Type = SlotType.Player
+					}
+				}
+			};
+
+			var itemInfoResolver = Substitute.For<IItemInfoResolver>();
+			var connectionInfoProvider = Substitute.For<IConnectionInfoProvider>();
+			connectionInfoProvider.Slot.Returns(7);
+			connectionInfoProvider.Team.Returns(20);
+			var playerInfoProvider = Substitute.For<IPlayerHelper>();
+			playerInfoProvider.GetPlayerInfo(connectionInfoProvider.Team, networkItem.Player).Returns(sender);
+			playerInfoProvider.GetPlayerInfo(connectionInfoProvider.Team, connectionInfoProvider.Slot).Returns(currentPlayer);
+			var session = CreateTestSession(itemInfoResolver, playerInfoProvider, connectionInfoProvider);
+
+			var sut = scouted
+				? new ScoutedItemInfo(networkItem, receiverGame, senderGame, itemInfoResolver, currentPlayer)
+				: new ItemInfo(networkItem, receiverGame, senderGame, itemInfoResolver, sender);
+				
+			var json = sut.ToSerializable().ToJson(fullJson);
+			var deserialized = SerializableItemInfo.FromJson(json, provideSession ? session : null);
+
+			Assert.IsNotNull(deserialized);
+			Assert.That(deserialized.ItemId, Is.EqualTo(sut.ItemId));
+			Assert.That(deserialized.LocationId, Is.EqualTo(sut.LocationId));
+			Assert.That(deserialized.PlayerSlot, Is.EqualTo(sut.Player.Slot));
+			Assert.That(deserialized.Flags, Is.EqualTo(sut.Flags));
+			Assert.That(deserialized.ItemGame, Is.EqualTo(sut.ItemGame));
+			Assert.That(deserialized.LocationGame, Is.EqualTo(sut.LocationGame));
+		}
+
+		IArchipelagoSession CreateTestSession(IItemInfoResolver itemInfoResolver, IPlayerHelper playerInfoProvider, IConnectionInfoProvider connectionInfo)
+		{
+			var socket = Substitute.For<IArchipelagoSocketHelper>();
+
+			var locations = new LocationCheckHelper(socket, itemInfoResolver, connectionInfo, playerInfoProvider);
+			var item = new ReceivedItemsHelper(socket, locations, itemInfoResolver, connectionInfo, playerInfoProvider);
+
+			var session = Substitute.For<IArchipelagoSession>();
+			session.Players.Returns(playerInfoProvider);
+			session.ConnectionInfo.Returns(connectionInfo);
+			session.Locations.Returns(locations);
+			session.Items.Returns(item);
+
+			return session;
+		}
+	}
+}
+
+
---
+++ Archipelago.MultiClient.Net/Models/SerializableItemInfo.cs
@@ -0,0 +1,161 @@
+﻿using Archipelago.MultiClient.Net.DataPackage;
+using Archipelago.MultiClient.Net.Enums;
+using Archipelago.MultiClient.Net.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Runtime.Serialization;
+
+
+namespace Archipelago.MultiClient.Net.Models
+{
+	public class MinimalSerializableItemInfo
+	{
+		/// <summary>
+		/// The item id of the item. Item ids are in the range of ± (2^53)-1.
+		/// </summary>
+		public long ItemId { get; set; }
+
+		/// <summary>
+		/// The location id of the item inside the world. Location ids are in the range of ± (2^53)-1.
+		/// </summary>
+		public long LocationId { get; set; }
+
+		/// <summary>
+		/// The player slot
+		/// </summary>
+		public int PlayerSlot { get; set; }
+
+		/// <summary>
+		/// Enum flags that describe special properties of the Item
+		/// </summary>
+		public ItemFlags Flags { get; set; }
+
+		/// <summary>
+		/// The game the item belongs to
+		/// </summary>
+		public string ItemGame { get; set; }
+
+		/// <summary>
+		/// The game the location belongs to
+		/// </summary>
+		public string LocationGame { get; set; }
+	}
+
+	public class SerializableItemInfo : MinimalSerializableItemInfo
+	{
+		/// <summary>
+		/// Whether or not this ItemInfo came from an location scout
+		/// </summary>
+		public bool IsScout { get; set; }
+
+		/// <summary>
+		/// The player of the world the item is located in, unless IsScout is true than its the player who will receive the item
+		/// </summary>
+		public PlayerInfo Player { get; set; }
+
+		/// <summary>
+		/// The name of the item, null if the name cannot be resolved
+		/// </summary>
+		public string ItemName { get; set; }
+
+		/// <summary>
+		/// The name of the location that item is at, null if the name cannot be resolved
+		/// </summary>
+		public string LocationName { get; set; }
+		
+		/// <summary>
+		/// The name of the item for display purposes, returns a fallback string containing the ItemId if the name cannot be resolved 
+		/// </summary>
+		[JsonIgnore]
+		public string ItemDisplayName => ItemName ?? $"Item: {ItemId}";
+
+		/// <summary>
+		/// The name of the location for display purposes, returns a fallback string containing the LocationId if the name cannot be resolved 
+		/// </summary>
+		[JsonIgnore]
+		public string LocationDisplayName => LocationName ?? $"Location: {LocationId}";
+
+		public string ToJson(bool full)
+		{
+			MinimalSerializableItemInfo objectToSerialize = this;
+
+			if (!full)
+			{
+				objectToSerialize = new MinimalSerializableItemInfo {
+					ItemId = ItemId,
+					LocationId = LocationId,
+					PlayerSlot = PlayerSlot,
+					Flags = Flags,
+				};
+
+				if (IsScout)
+					objectToSerialize.ItemGame = ItemGame;
+				else
+					objectToSerialize.LocationGame = LocationGame;
+			}
+
+			var serializerSettings = new JsonSerializerSettings {
+				NullValueHandling = NullValueHandling.Ignore,
+				Formatting = Formatting.None
+			};
+			return JsonConvert.SerializeObject(objectToSerialize, serializerSettings);
+		}
+
+
+		public static SerializableItemInfo FromJson(string json, IArchipelagoSession session = null)
+		{
+			if (session == null)
+				return JsonConvert.DeserializeObject<SerializableItemInfo>(json);
+
+			var streamingContext = new ItemInfoStreamingContext {
+				Items = session.Items,
+				Locations = session.Locations,
+				PlayerHelper = session.Players,
+				ConnectionInfo = session.ConnectionInfo
+			};
+
+			var serializerSettings = new JsonSerializerSettings
+			{
+				Context = new StreamingContext(StreamingContextStates.Other, streamingContext)
+			};
+
+			return JsonConvert.DeserializeObject<SerializableItemInfo>(json, serializerSettings);
+		}
+
+		[OnDeserialized]
+		internal void OnDeserializedMethod(StreamingContext streamingContext)
+		{
+			var context = streamingContext.Context as ItemInfoStreamingContext;
+			if (context == null)
+				return;
+
+			if (ItemGame == null && LocationGame != null)
+			{
+				IsScout = false;
+				ItemGame = context.ConnectionInfo.Game;
+			}
+			else if (ItemGame != null && LocationGame == null)
+			{
+				IsScout = true;
+				LocationGame = context.ConnectionInfo.Game;
+			}
+
+			if (ItemName == null)
+				ItemName = context.Items.GetItemName(ItemId, ItemGame);
+
+			if (LocationName == null)
+				LocationName = context.Locations.GetLocationNameFromId(LocationId, LocationGame);
+
+			if (Player == null)
+				Player = context.PlayerHelper.GetPlayerInfo(PlayerSlot);
+		}
+	}
+
+	class ItemInfoStreamingContext
+	{
+		public IReceivedItemsHelper Items { get; set; }
+		public ILocationCheckHelper Locations { get; set; }
+		public IPlayerHelper PlayerHelper { get; set; }
+		public IConnectionInfoProvider ConnectionInfo { get; set; }
+	}
+}
