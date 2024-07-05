19089efb5fcdf8fa5b9cf3bde224648054775230
Jarno Westhof <jarnowesthof@gmail.com>
2024-07-05 20:34:47 +0200

    Added unit test for sending UTF8

--- Archipelago.MultiClient.Net.Tests/BaseArchipelagoSocketHelperFixture.cs
+++ Archipelago.MultiClient.Net.Tests/BaseArchipelagoSocketHelperFixture.cs
@@ -1,9 +1,12 @@
 ﻿#if NET47 || NET48 || NET6_0
 using Archipelago.MultiClient.Net.Helpers;
 using Archipelago.MultiClient.Net.Packets;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
@@ -67,12 +70,61 @@ namespace Archipelago.MultiClient.Net.Tests
 
 			Assert.That(receivedPacket, Is.Null);
 			Assert.That(error, Is.Not.Null);
+			Assert.That(error, Is.TypeOf(typeof(JsonReaderException)));
+		}
+
+		[TestCase("some message", 100, Description = "Buffer bigger than message")]
+		[TestCase("some message", 10, Description = "Buffer smaller than message")]
+		[TestCase("🃁🃂🃃🃄🃅🃆🃇🃈🃉🃊", 10, Description = "UTF8 complex character breaking A")]
+		[TestCase("🃁🃂🃃🃄🃅🃆🃇🃈🃉🃊", 11, Description = "UTF8 complex character breaking B")]
+		[TestCase("🃁🃂🃃🃄🃅🃆🃇🃈🃉🃊", 12, Description = "UTF8 complex character breaking C")]
+		[TestCase("🃁🃂🃃🃄🃅🃆🃇🃈🃉🃊", 13, Description = "UTF8 complex character breaking D")]
+		public async Task Should_serialize_and_send_message_to_websocket(string message, int bufferSize)
+		{
+			var sayPacket = new SayPacket { Text = message };
+			
+			var testSocket = new TestWebSocket();
+
+			var sut = new BaseArchipelagoSocketHelper<TestWebSocket>(testSocket, bufferSize);
+
+			ArchipelagoPacketBase[] sendPackets = null;
+
+			sut.PacketsSent += packet => sendPackets = packet;
+
+			sut.StartPolling();
+
+			await sut.SendPacketAsync(sayPacket);
+			
+			int maxRetries = 100;
+			int retryCount = 0;
+			while (sendPackets == null && retryCount++ < maxRetries)
+				await Task.Delay(10);
+
+			Assert.That(sendPackets, Is.Not.Null);
+			Assert.That(sendPackets[0], Is.EqualTo(sayPacket));
+
+			var sendMessages = testSocket.GetWrittenMessages();
+
+			Assert.That(sendMessages, Is.Not.Null);
+
+			var json = @"[{""cmd"":""Say"",""text"":""$MESSAGE""}]".Replace("$MESSAGE", message);
+			Assert.That(sendMessages[0], Is.EqualTo(json));
 		}
 	}
 
 	class TestWebSocket : WebSocket
 	{
 		MemoryStream incommingBytes;
+
+		int currentOutIndex;
+		readonly List<List<byte>> outBytes = new List<List<byte>> { new List<byte>() };
+
+		public List<string> GetWrittenMessages() =>
+			outBytes.Select(bytes => Encoding.UTF8.GetString(bytes.ToArray())).ToList();
+
+		public TestWebSocket() : this(string.Empty)
+		{
+		}
 
 		public TestWebSocket(string inMessage)
 		{
@@ -100,6 +152,16 @@ namespace Archipelago.MultiClient.Net.Tests
 		public override async Task SendAsync(ArraySegment<byte> buffer, WebSocketMessageType messageType, bool endOfMessage,
 			CancellationToken cancellationToken)
 		{
+			if (messageType != WebSocketMessageType.Text)
+				return;
+
+			outBytes[currentOutIndex].AddRange(buffer);
+
+			if (endOfMessage)
+			{
+				outBytes.Add(new List<byte>());
+				currentOutIndex++;
+			}
 
 			await Task.CompletedTask;
 		}
