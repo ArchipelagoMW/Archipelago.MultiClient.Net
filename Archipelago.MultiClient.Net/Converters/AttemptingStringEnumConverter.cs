2b74c3287e85b9da68d86b8b706344f6f4a49ce2
Fabian Dill <fabian.dill@web.de>
2025-07-14 19:36:22 +0200

    Return null for unknown JsonMessagePartType

---
+++ Archipelago.MultiClient.Net/Converters/AttemptingStringEnumConverter.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+public class AttemptingStringEnumConverter : StringEnumConverter
+{
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        try
+        {
+            return base.ReadJson(reader, objectType, existingValue, serializer);
+        }
+        catch (JsonSerializationException)
+        {
+            return null;
+        }
+    }
+}
